51c3e0ed39428fdd6b0c514db7cf28e09e1e174b
Andew <80929367+monomine0000@users.noreply.github.com>
2023-07-31 23:40:24 +0300

    fixes and decorations added

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,16 +1,15 @@
-import { PrismaClient } from "@prisma/client";
-
-// PrismaClient is attached to the `global` object in development to prevent
-// exhausting your database connection limit.
-//
-// Learn more: 
-// https://pris.ly/d/help/next-js-best-practices
+import 
+{ 
+  PrismaClient 
+} 
+ from "@prisma/client";
 
 let prisma: PrismaClient
 
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient()
-} else {
+}
+else {
   if (!global.prisma) {
     global.prisma = new PrismaClient()
   }
